72a7bf5c037fe9d54380f4452a20e1545afa07eb
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-07-26 23:06:56 +0500

    skelet completed

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  GUEST = 'guest',
+}
+
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({ nullable: true, select: false })
+  refreshToken: string | null;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  role: UserRole;
+
+}
