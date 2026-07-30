8bae81a219982e6b4cec351d209c63fd56314d62
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-07-30 13:27:03 +0500

    back of qr

--- src/registration/registration.module.ts
+++ src/registration/registration.module.ts
@@ -4,11 +4,12 @@ import { Registration } from './entities/registration.entity';
 import { Event } from '../event/entities/event.entity';
 import { RegistrationService } from './registration.service';
 import { RegistrationController } from './registration.controller';
+import { TicketService } from './ticket.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Registration, Event])],
   controllers: [RegistrationController],
-  providers: [RegistrationService],
+  providers: [RegistrationService, TicketService],
   exports: [RegistrationService],
 })
 export class RegistrationModule {}
